4423ee4c8700af9e6bd1b315b13b80ca04b0c304
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-11-16 09:10:08 -0600

    added components to App.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,22 @@
-import Topbar from "./components/Tobar/Topbar"
+import Topbar from "./components/Topbar/Topbar"
+import Intro from "./components/Intro/Intro"
+import Portfolio from "./components/Portfolio/Portfolio";
+import Works from "./components/Works/Works";
+import Contact from "./components/Contact/Contact";
+
+
 
 
 function App() {
   return (
     <div className="App">
-    <Topbar />
-    
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Contact />
+      </div>
     </div>
   );
 }
